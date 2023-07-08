97ba7e1f1979c08ebef384e015271d5ae26d5a1a
gurjaspreet <107759543+gurjaspreet@users.noreply.github.com>
2023-07-08 22:03:41 +0530

    Day 3 completed :)

---
+++ Day 3/Exercise2.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+arr = np.zeros((4, 4), dtype=int)
+print(arr)
---
+++ Day 3/Exercise3.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.full((10, 10), 255, dtype=float)
+
+# Alternative way:
+# arr = np.ones((10, 10), dtype=float) * 255
+
+print(arr)
